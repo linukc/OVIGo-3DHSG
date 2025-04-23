027616f333e0358a060aca84088b83b17e85c3d8
Gleb Naumov <naumovgm@mail.ru>
2025-04-23 12:01:29 +0300

    add dataset

---
+++ script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+echo "Sonverting COCO to YOLO format..."
+python3 paper_script.py --coco_root coco_locations/ --yolo_dataset_output_dir yolo_locations_dataset/
+
+echo "Starting YOLO training..."
+yolo segment train data=yolo_locations_dataset/yolo_root.yaml model=yolo11m-seg.pt epochs=300 imgsz=256 plots=True batch=-1
